d09a3271b02cb56750aa8281cb5d6d6a6ad82c75
karangupta8 <karangupta899@gmail.com>
2025-09-06 09:14:58 +0530

    Added Package icon import to CategoryManager for enhanced UI functionality.

--- components/packing/CategoryManager.tsx
+++ components/packing/CategoryManager.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { Plus, Edit2, Trash2, X } from 'lucide-react';
+import { Plus, Edit2, Trash2, X, Package } from 'lucide-react';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
